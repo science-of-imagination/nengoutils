b08d4095a89317303ce7da5745dfea2c41747a21
cmekik <can.mekik@gmail.com>
2015-02-13 15:51:23 -0500

    utils.analyze changed to comply with PEP257.

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,18 +1,36 @@
+"""This module provides tools for analyzing data.
+
+Provides:
+cosine -- Computes the cosine of the angle between two vectors.
+rmsd -- Computes the root mean square of the difference of two vectors.
+"""
 from tkFileDialog import askopenfilenames
 from pylab import plot, show
 from data import load_data
 from numpy import dot, mean, sqrt, subtract
 from numpy.linalg import norm
 
-def cos_simi(tgt, opt):
-    """Return cosine similarity score for arrays tgt, opt.
+def cosine(u, v):
+    """Compute and return the cosine of the angle between the vectors u, v.
 
-    Note: Computes <tgt, opt>/(|tgt||opt|)."""
-    return dot(tgt, opt)/(norm(tgt)*norm(opt))
+    Note: Computes the ratio of the dot product of the two vectors to the
+    product of the norms of the two vectors (<tgt, opt>/(|tgt||opt|)).
 
-def rmse(tgt, opt):
-    """Return the rmse between arrays tgt, opt."""
-    return sqrt(mean(dot(subtract(tgt, opt), subtract(tgt, opt))))
+    Keyword arguments:
+    u -- array_like, the first vector.
+    v -- array_like, the second vector."""
+
+    return dot(u, v)/(norm(u)*norm(v))
+
+def rmsd(u, v):
+    """Return the root mean square of the difference between vectors u, v.
+
+    Keyword arguments:
+    u -- array_like, the first vector.
+    v -- array_like, the second vector."""
+
+    d = subtract(u, v)
+    return sqrt(mean(dot(d, d)))
 
 #queue = askopenfilenames()
 #if isinstance(queue, unicode):
