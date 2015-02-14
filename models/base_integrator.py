b8569faa94fcc53f32358d6370f13c78c0bacfc0
cmekik <can.mekik@gmail.com>
2015-02-13 20:41:46 -0500

    experiment_0 and models.base_integrator changed to comply with PEP257.

--- models/base_integrator.py
+++ models/base_integrator.py
@@ -1,32 +1,31 @@
+"""Provides a nengo neural integrator model."""
+
 import nengo
 
 
-def model(N, input_dims, encs, eval_points, stim_func, conn_synapse=0.1,
+def model(n, input_dims, encs, eval_points, stim_func, conn_synapse=0.1,
           probe_synapse=0.01):
-    """Initialize and build a neural integrator. Returns a tuple (sim, probe)
-    where sim is the nengo.Simulator() object resulting from the arguments
-    passed to the function and probe is the nengo.Probe() object which probes
-    the decoded outputs of the integrator ensemble.
+    """Initialize and build a neural integrator.
 
-    Parameters:
+    Returns a tuple (sim, probe) where sim is the nengo.Simulator() object
+    resulting from the arguments passed to the function and probe is the
+    nengo.Probe() object which probes the decoded outputs of the integrator
+    ensemble.
 
-    N: Number of neurons in the ensemble.
+    Keyword arguments:
 
-    input_dims: Side length of the input image(s) (the image is assumed to be
-        square).
-
-    encs: Encoders for the ensemble.
-
-    eval_points: Evaluation points for the ensemble.
-
-    stim_func: A stimulus function. Should be a function that takes one
-        argument, a float representing simulation time in seconds, and that
-        returns the stimulus to the network at that time.
-
-    conn_synapse: PSTC (in seconds) for the recurrent connection of the
-        ensemble.
-
-    probe_synapse: PSTC (in seconds) for the probe."""
+    n -- int, number of neurons in the ensemble.
+    input_dims -- int, side length of the input image(s) (the image(s) are
+        assumed
+        to be square).
+    encs -- array_like, encoders for the ensemble.
+    eval_points -- array_like, evaluation points for the ensemble.
+    stim_func -- function, a stimulus function. Should be a function that
+        takes one argument, a float representing simulation time in seconds,
+        and that returns the stimulus to the network at that time.
+    conn_synapse -- float, PSTC (in seconds) for the recurrent connection of
+        the model.
+    probe_synapse: float, PSTC (in seconds) for the probe."""
 
     print 'Building model.'
     with nengo.Network() as net:
@@ -34,7 +33,7 @@ def model(N, input_dims, encs, eval_points, stim_func, conn_synapse=0.1,
         neuron_type = nengo.LIF()        
 
         ipt = nengo.Node(stim_func)
-        ens = nengo.Ensemble(N,
+        ens = nengo.Ensemble(n,
                              dimensions=input_dims,
                              encoders=encs,
                              eval_points=eval_points,
